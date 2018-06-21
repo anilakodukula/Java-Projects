23d40ff50d3e56c0e0c9c273002acc1dbb24171d
anilakodukula <35850162+anilakodukula@users.noreply.github.com>
2018-06-21 18:51:36 -0400

    Email Application
    This app is designed to created email accounts for new hires. The requirements include :

---
+++ EmailApp.java
@@ -0,0 +1,11 @@
+package com.emailApp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		Email em1 = new Email(" Anila ", " Kodukula "); // constructor
+		System.out.println(em1.showInfo());
+		
+	}
+
+}
